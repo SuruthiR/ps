46a3ee31ea6af5e4601dafaf03bd57cc00157354
SuruthiR <47344181+SuruthiR@users.noreply.github.com>
2019-02-05 12:19:45 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,6 @@
+ch = input("enter your character:")
+if(ch == 'a' or ch =='e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
+    print("Given character",ch,"is a vowel")
+else:
+    print("given character",ch,"is a consonant")
+    
